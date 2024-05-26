155169637b06ee85df7fc27e4d5cc4756a91b6b1
Daniel Mao <danielmao2019@gmail.com>
2024-05-26 18:11:52 -0400

    Implement AAAI scraper

---
+++ scraper/scraper_aaai.py
@@ -0,0 +1,36 @@
+from typing import Dict
+from . import utils
+
+
+def scrape_aaai(url: str) -> Dict[str, str]:
+    assert type(url) == str, f"{type(url)=}"
+    soup = utils.get_soup(url)
+    # get title
+    title = soup.findAll(name='meta', attrs={'name': 'DC.Title'})
+    assert len(title) == 1
+    title = title[0]['content']
+    # get pdf url
+    pdf_url = soup.findAll(name='a', attrs={'class': 'obj_galley_link pdf'})
+    assert len(pdf_url) == 1, f"{pdf_url=}"
+    pdf_url = pdf_url[0]['href']
+    # get year
+    year = soup.findAll(name='meta', attrs={'name': 'DC.Date.created'})
+    assert len(year) == 1, f"{year=}"
+    year = f"`{year[0]['content'].split('-')[0]}`"
+    # get authors
+    authors = soup.findAll(name='meta', attrs={'name': 'DC.Creator.PersonalName'})
+    authors = ", ".join([t['content'] for t in authors])
+    # get abstract
+    abstract = soup.findAll(name='meta', attrs={'name': 'DC.Description'})
+    assert len(abstract) == 1
+    abstract = abstract[0]['content']
+    # return
+    return {
+        'title': title,
+        'abs_url': url,
+        'pdf_url': pdf_url,
+        'pub_name': "AAAI",
+        'pub_year': year,
+        'authors': authors,
+        'abstract': abstract,
+    }
