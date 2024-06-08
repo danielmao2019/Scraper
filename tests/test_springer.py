65f76c0f497f016b5aa6a166c24c214aa78ca89d
Daniel Mao <danielmao2019@gmail.com>
2024-06-08 15:35:24 -0400

    Implement test case for Springer scraper

---
+++ tests/test_springer.py
@@ -0,0 +1,20 @@
+import pytest
+import sys
+sys.path.append("..")
+from scraper import scrape
+import os
+
+
+@pytest.mark.parametrize("url, expected", [
+    (
+        "https://link.springer.com/chapter/10.1007/978-3-319-13560-1_76",
+        "test_springer_pricai.txt",
+    ),
+])
+def test_springer(url: str, expected: str) -> None:
+    assert type(url) == str, f"{type(url)=}"
+    assert type(expected) == str, f'{expected=}'
+    assert os.path.isfile(expected), f"{expected=}"
+    with open(expected, mode='r') as f:
+        produced = scrape(url)
+        assert produced == f.read(), f"{produced=}"
