8d09acb2f6bfd4429d90f07f7eed4dad549619ce
Daniel Mao <danielmao2019@gmail.com>
2024-11-23 20:44:34 -0500

    Add user agent and other headers when using selenium

--- utils/beautiful_soup.py
+++ utils/beautiful_soup.py
@@ -7,6 +7,17 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 import time
 
+import random
+
+
+def get_random_user_agent():
+    user_agents = [
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.6778.85 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.6778.85 Safari/537.36",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.6778.85 Safari/537.36"
+    ]
+    return random.choice(user_agents)
+
 
 def get_soup_with_urllib(url: str, headers: dict):
     ssl_context = ssl.create_default_context()
@@ -28,6 +39,14 @@ def get_soup_with_selenium(url: str):
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument(f"user-agent={get_random_user_agent()}")
+    options.add_argument("accept-language=en-US,en;q=0.9")
+    options.add_argument("upgrade-insecure-requests=1")
+    options.add_argument("sec-ch-ua=\"Google Chrome\";v=\"118\", \"Not A(Brand\";v=\"8\", \"Chromium\";v=\"118\"")
+    options.add_argument("sec-ch-ua-mobile=?0")
+    options.add_argument("sec-ch-ua-platform=\"Windows\"")
+    options.add_argument("dnt=1")  # Do Not Track
+    options.add_argument("accept=text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
 
     # Update the path to your ChromeDriver executable
     driver = webdriver.Chrome(service=Service("/usr/local/bin/chromedriver"), options=options)
